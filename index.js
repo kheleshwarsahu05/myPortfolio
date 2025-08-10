5f2e82be535851fa4cd080791370242bd8443ce6
kheleshwarsahu05 <kheleshwar.dev@gmail.com>
2025-08-10 21:13:21 +0530

    Home and About Section is Almost Completed

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ const darkMode = document.getElementById("dark-mode");
 const modeTxt = document.getElementById("modeTxt");
 const nav = document.getElementById("nav");
 const body = document.getElementById("body");
+const round = document.getElementById("round");
+const navImgContainer = document.getElementsByClassName("navImgContainer");
+const heroTxt = document.getElementById("heroTxt");
 
-const toggle = ()=>{
-    if(darkMode.style.flexDirection == "row"){
-        darkMode.style.flexDirection = "row-reverse";
-        modeTxt.textContent = "Dark Mode";
-        nav.style.backgroundColor = "white";
-        nav.style.color = "black"
-        body.style.backgroundColor = "#c9c9c9d1";
+    const toggle = ()=>{
+        if(round.style.marginLeft === "2px" || round.style.marginLeft === ""){
+            round.style.marginLeft = "32px";
+            round.style.border = "1px solid";
+            round.style.marginRight = "2px";
+            round.style.backgroundColor = "black";
+        }else{
+             round.style.marginLeft = "2px";
+             round.style.backgroundColor = "white";
+        }
 
-    }else{
-        darkMode.style.flexDirection = "row";
-        modeTxt.textContent = "Light Mode";
-        nav.style.backgroundColor = "black";
-        nav.style.color = "white";
-        body.style.backgroundColor = "#000000d1";
-
+        if(round.style.marginLeft === "2px" || round.style.marginLeft === ""){
+            body.style.backgroundImage = "url(sl_0210121_40570_43.jpg)";
+            darkMode.style.backgroundColor = "black";
+            nav.style.color = "white";
+            heroTxt.style.color = "black";
+        }else{
+            body.style.backgroundImage = "none";
+            body.style.backgroundColor = "white";
+            nav.style.color = "black";
+            darkMode.style.backgroundColor = "white";
+            navImgContainer.style.borderColor = "white";
+            heroTxt.style.color = "white";
+        }
     }
-}
